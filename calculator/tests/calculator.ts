6e7edd686dfd40868a634322bc1212df979cdf7c
rabeebaqdas <rabeebaqdas@gmail.com>
2024-04-30 20:38:30 +0500

    calculator

---
+++ calculator/tests/calculator.ts
@@ -0,0 +1,37 @@
+
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
+
+describe("calculator", () => {
+  const provider = anchor.Provider.local();
+  anchor.setProvider(provider);
+  const calculator = anchor.web3.Keypair.generate();
+  const program = anchor.workspace.Calculator;
+
+  it("Creates a calculator", async () => {
+    await program.rpc.create("Welcome to solana", {
+      accounts: {
+        calculator: calculator.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [calculator],
+    });
+    const account = await program.account.calculator.fetch(
+      calculator.publicKey
+    );
+    assert.ok(account.greeting === "Welcome to solana");
+  });
+  it("Add two numbers", async () => {
+    await program.rpc.add(new anchor.BN(2), new anchor.BN(3), {
+      accounts: {
+        calculator: calculator.publicKey,
+      },
+    });
+    const account = await program.account.calculator.fetch(
+      calculator.publicKey
+    );
+    assert.ok(account.result.eq(new anchor.BN(5)));
+  });
+});
