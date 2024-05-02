7458557b9c6f1b364535ab72eb8042e1f4aacb9b
rabeebaqdas <rabeebaqdas@gmail.com>
2024-05-02 20:06:35 +0500

    get compaign

--- crowdfunding/frontend/src/App.js
+++ crowdfunding/frontend/src/App.js
@@ -2,13 +2,7 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import idl from "./utils/idl.json";
 import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
-import {
-  Program,
-  AnchorProvider,
-  web3,
-  utils,
-  BN,
-} from "@project-serum/anchor";
+import { Program, AnchorProvider, web3, utils } from "@project-serum/anchor";
 import { Buffer } from "buffer";
 window.Buffer = Buffer;
 const { solana } = window;
@@ -20,8 +14,10 @@ const opts = {
 const { SystemProgram } = web3;
 function App() {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [campaigns, setCampaigns] = useState([]);
 
   const getProvider = () => {
+    //Creating a provider, the provider is authenication connection to solana
     const connection = new Connection(network, opts.preflightCommitment);
     const provider = new AnchorProvider(
       connection,
@@ -30,6 +26,7 @@ function App() {
     );
     return provider;
   };
+
   const checkIsWalletConnected = async () => {
     try {
       if (!solana) return alert("please install Phantom");
@@ -54,11 +51,30 @@ function App() {
     }
   };
 
+  const getCompaigns = async () => {
+    try {
+      const connection = new Connection(network, opts.preflightCommitment);
+      const provider = getProvider();
+
+      const program = new Program(idl, programID, provider);
+      Promise.all(
+        (await connection.getProgramAccounts(programID)).map(
+          async (campaign) => ({
+            ...(await program.account.campaign.fetch(campaign.pubkey)),
+            pubkey: campaign.pubkey,
+          })
+        )
+      ).then((campaign) => setCampaigns(campaign));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const createCompaign = async () => {
     try {
       const provider = getProvider();
       const program = new Program(idl, programID, provider);
-      console.log(program, "program");
+      console.log(programID, "program");
       const [campaign] = await PublicKey.findProgramAddress(
         [
           utils.bytes.utf8.encode("CAMPAIGN_DEMO"),
@@ -95,7 +111,23 @@ function App() {
       <button onClick={connectWallet} disabled={currentAccount !== ""}>
         {currentAccount !== "" ? currentAccount : "Connect Wallet"}
       </button>
+      <br />
       <button onClick={createCompaign}>Create Compaign</button>
+      <button onClick={getCompaigns}>Get Compaigns List</button>
+
+      <br />
+      {campaigns.map((campaign) => (
+        <>
+          <p>Campaign ID: {campaign.pubkey.toString()}</p>
+          <p>Campaign Name: {campaign.name}</p>
+          <p>Campaign ID: {campaign.description}</p>
+          <p>
+            Amount Donated:{" "}
+            {(campaign.amountDonated / web3.LAMPORTS_PER_SOL).toString()}
+          </p>
+          <br />
+        </>
+      ))}
     </div>
   );
 }
