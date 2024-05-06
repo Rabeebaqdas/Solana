100f597c8b1c4e428384d2e905b278b45249e484
rabeebaqdas <rabeebaqdas@gmail.com>
2024-05-06 15:03:37 +0500

    backend initiated

--- giphy/gifportal/tests/gifportal.ts
+++ giphy/gifportal/tests/gifportal.ts
@@ -2,7 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 const assert = require("assert");
 const { SystemProgram } = anchor.web3;
 
-describe("calculator", async() => {
+describe("Giphy", async() => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.Gifportal;
