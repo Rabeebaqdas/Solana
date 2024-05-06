e42affbdbe7ac2b8061ff0806406361607c2b46f
rabeebaqdas <rabeebaqdas@gmail.com>
2024-05-06 15:26:00 +0500

    account initialized

--- giphy/gifportal/programs/gifportal/src/lib.rs
+++ giphy/gifportal/programs/gifportal/src/lib.rs
@@ -12,4 +12,17 @@ pub mod gifportal {
 }
 
 #[derive(Accounts)]
-pub struct StartStuffOff{}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
