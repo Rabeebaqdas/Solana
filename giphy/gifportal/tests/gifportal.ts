76dde7c3600a2e0fa91f4a51a26e02f349aff3c1
rabeebaqdas <rabeebaqdas@gmail.com>
2024-05-06 16:44:22 +0500

    testing completed

--- giphy/gifportal/tests/gifportal.ts
+++ giphy/gifportal/tests/gifportal.ts
@@ -1,12 +1,36 @@
-import * as anchor from "@coral-xyz/anchor";  
+import * as anchor from "@coral-xyz/anchor";
 const assert = require("assert");
 const { SystemProgram } = anchor.web3;
 
-describe("Giphy", async() => {
+describe("Giphy", async () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
-  const program = anchor.workspace.Gifportal;
-  const tx = await program.rpc.startStuffOff()
-  console.log("Signature Tx",tx);
 
+  const program = anchor.workspace.Gifportal;
+  const giphy = anchor.web3.Keypair.generate();
+
+  it("Creates a Giphy", async () => {
+    const tx = await program.rpc.startStuffOff({
+      accounts: {
+        baseAccount: giphy.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [giphy],
+    });
+    let account = await program.account.baseAccount.fetch(giphy.publicKey);
+    assert.ok(account.totalGifs.eq(new anchor.BN(0)));
+  });
+
+  it("Add Gif", async () => {
+    await program.rpc.addGif("https://i.gifer.com/Be.gif", {
+      accounts: {
+        baseAccount: giphy.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    });
+    const account = await program.account.baseAccount.fetch(giphy.publicKey);
+    console.log("Gif List", account.gifList);
+    assert.ok(account.totalGifs.eq(new anchor.BN(1)));
+  });
 });
