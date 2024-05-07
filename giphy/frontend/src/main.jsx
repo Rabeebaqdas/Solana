403f5009a5578ba3a73c1dbbcbe4cebfb774def6
rabeebaqdas <rabeebaqdas@gmail.com>
2024-05-07 18:32:18 +0500

    changes in main

--- giphy/frontend/src/main.jsx
+++ giphy/frontend/src/main.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// main.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
+// Find the root element in the HTML
+const rootElement = document.getElementById("root");
+
+// Create the root using the new API
+const root = createRoot(rootElement);
+
+// Use the new root to render your application
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
