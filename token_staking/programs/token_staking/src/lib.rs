637ba3b55ed5f360033cc9f4ce3558f7cd7829bb
rabeebaqdas <rabeebaqdas@gmail.com>
2024-05-13 19:19:06 +0500

    added constants

--- token_staking/programs/token_staking/src/lib.rs
+++ token_staking/programs/token_staking/src/lib.rs
@@ -2,6 +2,12 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+pub mod constants {
+    pub const VAULT_SEED: &[u8] = b"vault";
+    pub const STAKE_INFO_SEED: &[u8] = b"stake_info";
+    pub const TOKEN_SEED: &[u8] = b"token";
+}
+
 #[program]
 pub mod token_staking {
     use super::*;
@@ -12,4 +18,21 @@ pub mod token_staking {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer : Signer<'info>,
+    
+    #[account(
+        init_if_needed,
+        seeds = [constants::VAULT_SEED],
+        bump,
+        payer = signer, 
+        token::mint = mint,
+        token::authority = token_vault_account,
+
+    )]
+    pub token_vault_account: Account<'info, TokenAccount>,
+    pub mint: Account<'info, MintAccount>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
