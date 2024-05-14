d2b2885766803b0235cd6067ddde5431a887c997
rabeebaqdas <rabeebaqdas@gmail.com>
2024-05-14 21:53:02 +0500

    added new features

--- token_staking/programs/token_staking/src/lib.rs
+++ token_staking/programs/token_staking/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_spl :: {
+    associated_token::AssociatedToken,
     token:: {self, Mint, Token, TokenAccount}
 };
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
@@ -38,3 +39,62 @@ pub struct Initialize<'info> {
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
 }
+#[derive(Accounts)]
+pub struct Stake<'info> {
+    #[account(mut)]
+    pub signer : Signer<'info>,
+    
+    #[account(
+        init_if_needed,
+        seeds = [constants::STAKE_INFO_SEED, signer.key.as_ref()],
+        bump,
+        payer = signer, 
+        token::mint = mint,
+        token::authority = stake_info_account,
+
+    )]
+    pub stake_info_account: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        seeds = [constants::TOKEN_SEED, signer.key.as_ref()],
+        bump,
+        payer = signer, 
+        token::mint = mint,
+        token::authority = stake_account,
+
+    )]
+    pub stake_account: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+    )]
+    pub user_token_account: Account<'info, TokenAccount>,
+
+    pub mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account] 
+pub struct StakeInfo {
+    pub amount: u64,
+    pub is_staked : bool
+
+}
+
+
+#[error_code]
+pub enum  ErrorCode {
+    #[msg("Tokens are already staked")]
+    IsStaked,
+    #[msg("Tokens are not staked")]
+    NotStaked,
+    #[msg("No Tokens to stake")]
+    NoTokens,
+
+}
+
--- token_staking/tests/token_staking.ts
+++ token_staking/tests/token_staking.ts
@@ -1,16 +1,50 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { TokenStaking } from "../target/types/token_staking";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { createMint } from "@solana/spl-token";
 
 describe("token_staking", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const payer = provider.wallet as anchor.Wallet;
+  const connection = new Connection("http://127.0.0.1:8899", "confirmed");
+  // const mintKeyPair = Keypair.generate();
+  const mintKeyPair = Keypair.fromSecretKey(
+    new Uint8Array([
+      15, 22, 18, 141, 131, 191, 84, 178, 190, 130, 9, 121, 246, 131, 81, 236,
+      238, 224, 223, 92, 57, 209, 20, 43, 166, 49, 79, 80, 223, 91, 235, 227,
+      61, 110, 244, 67, 190, 117, 255, 231, 69, 82, 202, 17, 154, 106, 195, 225,
+      204, 93, 15, 67, 81, 236, 31, 136, 118, 30, 142, 131, 114, 109, 81, 77,
+    ])
+  );
+  console.log(mintKeyPair);
 
   const program = anchor.workspace.TokenStaking as Program<TokenStaking>;
 
+  // async function createMintToken() {
+  //   const mint = await createMint(
+  //     connection,
+  //     payer.payer,
+  //     payer.publicKey,
+  //     payer.publicKey,
+  //     9,
+  //     mintKeyPair
+  //   );
+  //   console.log(mint);
+  // }
+
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    let [vaultAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("vault")],
+      program.programId
+    );
+    const tx = await program.methods.initialize().accounts({
+      signer: payer.publicKey,
+      tokenVaultAccount: vaultAccount,
+      mint: mintKeyPair.publicKey,
+    }).rpc();
     console.log("Your transaction signature", tx);
   });
 });
