4a07dc6599ee196bfb5365a419c5c2da838f6f37
rabeebaqdas <rabeebaqdas@gmail.com>
2024-05-15 20:35:37 +0500

    program completed

--- token_staking/programs/token_staking/src/lib.rs
+++ token_staking/programs/token_staking/src/lib.rs
@@ -1,12 +1,11 @@
 use anchor_lang::prelude::*;
 use anchor_spl :: {
     associated_token::AssociatedToken,
-    token:: {self, Mint, Token, TokenAccount, Transfer, transfer}
+    token:: { Mint, Token, TokenAccount, Transfer, transfer}
 };
 use solana_program::clock::Clock;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
-
+declare_id!("7XFbaKsugiPV3q6KLmpDdpydooFBAurqGyVWY3Zy2EZ9");
 pub mod constants {
     pub const VAULT_SEED: &[u8] = b"vault";
     pub const STAKE_INFO_SEED: &[u8] = b"stake_info";
@@ -50,6 +49,46 @@ pub mod token_staking {
             ),
             amount
         )?;
+
+        Ok(())
+    }
+
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        let stake_info = &mut ctx.accounts.stake_info_account;
+        if !stake_info.is_staked {
+            return Err(ErrorCode::NotStaked.into());
+        }
+        let clock = Clock::get()?;
+        let slots_passed = clock.slot - stake_info.staked_at_slot;
+        let stake_amount = ctx.accounts.stake_account.amount;
+
+        let reward = (slots_passed as u64).checked_mul(10u64.pow(ctx.accounts.mint.decimals as u32)).unwrap();
+        let bump_vault = ctx.bumps.token_vault_account;
+        let signer: &[&[&[u8]]] = &[&[constants::VAULT_SEED, &[bump_vault]]];
+        transfer(
+            CpiContext::new_with_signer(ctx.accounts.token_program.to_account_info(), Transfer{
+                from:ctx.accounts.token_vault_account.to_account_info() ,
+                to: ctx.accounts.user_token_account.to_account_info(),
+                authority:ctx.accounts.token_vault_account.to_account_info() ,
+            }, signer),
+            reward
+        )?;
+
+        let staker = ctx.accounts.signer.key();
+        let bump_stake = ctx.bumps.stake_account;
+        let signer: &[&[&[u8]]] = &[&[constants::TOKEN_SEED, staker.as_ref(),&[bump_stake]]];
+        transfer(
+            CpiContext::new_with_signer(ctx.accounts.token_program.to_account_info(), Transfer{
+                from:ctx.accounts.stake_account.to_account_info() ,
+                to: ctx.accounts.user_token_account.to_account_info(),
+                authority:ctx.accounts.stake_account.to_account_info() ,
+            }, signer),
+            stake_amount
+        )?;
+
+        stake_info.is_staked = false;
+        stake_info.staked_at_slot = clock.slot;
 
         Ok(())
     }
@@ -112,13 +151,51 @@ pub struct Stake<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct Unstake<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [constants::VAULT_SEED],
+        bump,
+    )]
+    pub token_vault_account: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [constants::STAKE_INFO_SEED, signer.key.as_ref()],
+        bump,
+    )]
+    pub stake_info_account: Account<'info, StakeInfo>,
+
+    #[account(
+        mut,
+        seeds = [constants::TOKEN_SEED, signer.key.as_ref()],
+        bump,
+    )]
+    pub stake_account: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+    )]
+    pub user_token_account: Account<'info, TokenAccount>,
+
+    pub mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
 #[account] 
 pub struct StakeInfo {
     pub staked_at_slot: u64,
     pub is_staked : bool
 
 }
-
 
 #[error_code]
 pub enum  ErrorCode {
--- token_staking/tests/token_staking.ts
+++ token_staking/tests/token_staking.ts
@@ -2,7 +2,11 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { TokenStaking } from "../target/types/token_staking";
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
-import { createMint } from "@solana/spl-token";
+import {
+  createMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+} from "@solana/spl-token";
 
 describe("token_staking", () => {
   // Configure the client to use the local cluster.
@@ -10,41 +14,149 @@ describe("token_staking", () => {
   anchor.setProvider(provider);
   const payer = provider.wallet as anchor.Wallet;
   const connection = new Connection("http://127.0.0.1:8899", "confirmed");
-  // const mintKeyPair = Keypair.generate();
-  const mintKeyPair = Keypair.fromSecretKey(
-    new Uint8Array([
-      15, 22, 18, 141, 131, 191, 84, 178, 190, 130, 9, 121, 246, 131, 81, 236,
-      238, 224, 223, 92, 57, 209, 20, 43, 166, 49, 79, 80, 223, 91, 235, 227,
-      61, 110, 244, 67, 190, 117, 255, 231, 69, 82, 202, 17, 154, 106, 195, 225,
-      204, 93, 15, 67, 81, 236, 31, 136, 118, 30, 142, 131, 114, 109, 81, 77,
-    ])
-  );
-  console.log(mintKeyPair);
+  const mintKeyPair = Keypair.generate();
+  // const mintKeyPair = Keypair.fromSecretKey(
+  //   new Uint8Array([
+  //     36, 70, 213, 113, 12, 69, 76, 247, 165, 75, 89, 69, 36, 158, 66, 13, 11,
+  //     197, 51, 189, 5, 76, 253, 128, 64, 147, 155, 113, 52, 48, 77, 96, 146, 31,
+  //     22, 148, 16, 218, 82, 4, 104, 45, 58, 98, 13, 58, 40, 42, 84, 15, 7, 103,
+  //     62, 72, 53, 145, 28, 188, 40, 132, 39, 97, 220, 241,
+  //   ])
+  // );
+  // console.log(mintKeyPair);
 
   const program = anchor.workspace.TokenStaking as Program<TokenStaking>;
 
-  // async function createMintToken() {
-  //   const mint = await createMint(
-  //     connection,
-  //     payer.payer,
-  //     payer.publicKey,
-  //     payer.publicKey,
-  //     9,
-  //     mintKeyPair
-  //   );
-  //   console.log(mint);
-  // }
+  async function createMintToken() {
+    const mint = await createMint(
+      connection,
+      payer.payer,
+      payer.publicKey,
+      payer.publicKey,
+      9,
+      mintKeyPair
+    );
+    console.log(mint);
+  }
 
   it("Is initialized!", async () => {
+    await createMintToken();
     let [vaultAccount] = PublicKey.findProgramAddressSync(
       [Buffer.from("vault")],
       program.programId
     );
-    const tx = await program.methods.initialize().accounts({
-      signer: payer.publicKey,
-      tokenVaultAccount: vaultAccount,
-      mint: mintKeyPair.publicKey,
-    }).rpc();
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        signer: payer.publicKey,
+        tokenVaultAccount: vaultAccount,
+        mint: mintKeyPair.publicKey,
+      })
+      .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("stake tokens!", async () => {
+    let userTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      payer.payer,
+      mintKeyPair.publicKey,
+      payer.publicKey
+    );
+
+    await mintTo(
+      connection,
+      payer.payer,
+      mintKeyPair.publicKey,
+      userTokenAccount.address,
+      payer.publicKey,
+      1e11
+    );
+
+    let [stakeInfo] = PublicKey.findProgramAddressSync(
+      [Buffer.from("stake_info"), payer.publicKey.toBuffer()],
+      program.programId
+    );
+
+    let [stakeAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("token"), payer.publicKey.toBuffer()],
+      program.programId
+    );
+
+    await getOrCreateAssociatedTokenAccount(
+      connection,
+      payer.payer,
+      mintKeyPair.publicKey,
+      payer.publicKey
+    );
+
+    const tx = await program.methods
+      .stake(new anchor.BN(1000000000))
+      .signers([payer.payer])
+      .accounts({
+        stakeInfoAccount: stakeInfo,
+        stakeAccount: stakeAccount,
+        userTokenAccount: userTokenAccount.address,
+        mint: mintKeyPair.publicKey,
+        signer: payer.publicKey,
+      })
+      .rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("unstake tokens!", async () => {
+    let userTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      payer.payer,
+      mintKeyPair.publicKey,
+      payer.publicKey
+    );
+
+    let [stakeInfo] = PublicKey.findProgramAddressSync(
+      [Buffer.from("stake_info"), payer.publicKey.toBuffer()],
+      program.programId
+    );
+
+    let [stakeAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("token"), payer.publicKey.toBuffer()],
+      program.programId
+    );
+
+    let [vaultAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("vault")],
+      program.programId
+    );
+
+    await getOrCreateAssociatedTokenAccount(
+      connection,
+      payer.payer,
+      mintKeyPair.publicKey,
+      payer.publicKey
+    );
+
+    //funding the vault so that it can give the rewards
+    await mintTo(
+      connection,
+      payer.payer,
+      mintKeyPair.publicKey,
+      vaultAccount,
+      payer.publicKey,
+      1e21
+    );
+
+
+    const tx = await program.methods
+      .unstake()
+      .signers([payer.payer])
+      .accounts({
+        stakeInfoAccount: stakeInfo,
+        stakeAccount: stakeAccount,
+        userTokenAccount: userTokenAccount.address,
+        tokenVaultAccount: vaultAccount,
+        mint: mintKeyPair.publicKey,
+        signer: payer.publicKey,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
